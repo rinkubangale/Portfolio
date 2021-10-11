1f7ced1e3e40c57e166db739abdb8c91d4a786ee
rinkubangale <bangalehemant@gmail.com>
2021-10-11 13:08:26 +0530

    contact page added

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import { FaPhoneAlt, FaAt } from 'react-icons/fa'
+import Styles from './Contact.css'
+
+
+export default function Contact() {
+    
+    return (
+        <div className="mainDiv">
+            <div>Rinku Bangale</div>
+            <div><FaPhoneAlt /> 7057215258</div>
+            <div><FaAt /> bangalehemant@gmail.com</div>
+
+        </div>
+    )
+}
--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,5 +1,6 @@
 import { Route } from "react-router-dom";
 import Home from './Home'
+import Contact from './Contact'
 
 export default function Routes() {
     
@@ -7,6 +8,7 @@ export default function Routes() {
         <Route exact path='/'><Home /></Route>
         <Route path='/aboutme'></Route>
         <Route path='/project'></Route>
-        <Route path='/contact'></Route>
+        <Route path='/contact'><Contact /></Route>
     </>)
+
 }
