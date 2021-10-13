f76e2bc80256316f54e1ad9296d1fb44d84afebe
rinkubangale <bangalehemant@gmail.com>
2021-10-13 14:05:17 +0530

    new Files Added

---
+++ src/components/Project.jsx
@@ -0,0 +1,22 @@
+export default function Project() {
+    
+    return (<>
+        <div style={{width: '100%', height: '100vh'}}>
+            <div style={{width: '33%', height: '50vh'}}>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <div>
+                            <div>Title</div>
+                        </div>
+                        <div>
+                            <div>Data here...</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>)
+}
